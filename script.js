b727c58b08f64eff9ce1f48e097b684a0a807e05
Julian <jamignott@gmail.com>
2020-05-15 16:23:29 -0400

    Complete File.

--- script.js
+++ script.js
@@ -8,11 +8,15 @@ $(document).ready(function () {
         $('#todo-list').empty();
         response.tasks.forEach(function (task) {
           $('#todo-list').append(
-            '<div class="row ml-3"><h5 class="col-xs-8">' +
+            '<div class="row ml-3"><h5 class="col-sm-10">' +
               task.content +
-              '</h5><button class="delete btn btn-sm btn-danger mb-3 ml-3" data-id="' +
+              '</h5><button class="delete btn btn-sm btn-danger mb-3 mx-3" data-id="' +
               task.id +
-              '">Delete</button>'
+              '">Delete</button><input type="checkbox" class="mark-complete mt-2" data-id="' +
+              task.id +
+              '"' +
+              (task.completed ? 'checked' : '') +
+              '>'
           );
         });
       },
@@ -68,5 +72,47 @@ $(document).ready(function () {
 
   $(document).on('click', '.delete', function () {
     deleteTask($(this).data('id'));
+  });
+
+  var markTaskComplete = function (id) {
+    $.ajax({
+      type: 'PUT',
+      url:
+        'https://altcademy-to-do-list-api.herokuapp.com/tasks/' +
+        id +
+        '/mark_complete?api_key=157',
+      dataType: 'json',
+      success: function (response, textStatus) {
+        console.log(response);
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      },
+    });
+  };
+
+  var markTaskActive = function (id) {
+    $.ajax({
+      type: 'PUT',
+      url:
+        'https://altcademy-to-do-list-api.herokuapp.com/tasks/' +
+        id +
+        '/mark_active?api_key=157',
+      dataType: 'json',
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      },
+    });
+  };
+
+  $(document).on('change', '.mark-complete', function () {
+    if (this.checked) {
+      markTaskComplete($(this).data('id'));
+    } else {
+      markTaskActive($(this).data('id'));
+    }
   });
 });
